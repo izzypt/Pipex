e2d234298f7228a224fea3fb8a4879345c2da247
Simao <bonvalots@gmail.com>
2023-05-28 22:46:23 +0100

    Creating pipes

---
+++ main.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/27 16:18:53 by simao             #+#    #+#             */
+/*   Updated: 2023/05/28 22:37:13 by simao            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int	main(int argc, char **argv)
+{
+	int	fd[2];
+	int	pid1;
+	int	pid2;
+
+	if (pipe(fd) == -1)
+		printf("Error on pipe()");
+	pid1 = fork();
+	if (pid1 == 0)
+	{
+		//Child process 1 (ping)
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("ping", "ping", "-c", "5", "google.com", NULL);
+	}
+	pid2 = fork();
+	if (pid2 == 0)
+	{
+		//child process 2 (grep)
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("grep", "grep", "rtt", NULL);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	return (0);
+}
