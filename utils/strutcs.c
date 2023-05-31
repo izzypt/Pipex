dca26c93061e7acef2c5f2e9fc75155a0368a5a1
Simao <bonvalots@gmail.com>
2023-05-31 21:57:04 +0100

    struct getters

--- utils/strutcs.c
+++ utils/strutcs.c
@@ -6,13 +6,13 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 12:25:38 by simao             #+#    #+#             */
-/*   Updated: 2023/05/31 15:22:20 by simao            ###   ########.fr       */
+/*   Updated: 2023/05/31 20:52:16 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-t_list	*cmds_lst(void)
+t_list	*cmd_list(void)
 {
 	static t_list	list;
 
@@ -23,7 +23,7 @@ t_data	*data(void)
 {
 	static t_data	dados;
 
-	dados.head = cmds_lst();
+	dados.head = cmd_list();
 
 	return (&dados);
 }
