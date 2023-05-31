58f85124d268004bba10aa068e1ae52ba3ee2e47
Simao <bonvalots@gmail.com>
2023-05-31 21:56:08 +0100

    header file

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 19:34:12 by simao             #+#    #+#             */
-/*   Updated: 2023/05/31 15:23:23 by simao            ###   ########.fr       */
+/*   Updated: 2023/05/31 20:51:07 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@
 
 typedef struct s_list
 {
-	char			*cmd;
-	int				fd[2];
 	char			*path;
+	char			*cmd;
 	char			**full_cmd;
-	struct s_list	*next;
-	struct s_list	*prev;
+	int				fd[2];
+	struct s_list	*nxt;
+	struct s_list	*prv;
 }				t_list;
 
 typedef struct s_data
@@ -36,18 +36,21 @@ typedef struct s_data
 	int		pid;
 	int		fd_input;
 	int		fd_output;
+	int		argc;
 	t_list	*head;
 	char	**paths;
 }	t_data;
 
 /* STRUCT UTILS */
-t_list	*cmds_lst(void);
+t_list	*cmd_list(void);
 t_data	*data(void);
 /* COMMAND FUNCTIONS */
+void	get_files_fd(int ac, char **av);
 void	run_command(void);
+void	parse_commands(char **argv);
 /* PATH FUNCTIONS */
 char	**get_path_list(char **env);
-int		path_acessible(char *cmd, char **paths);
+char	*valid_cmd_path(char *cmd);
 /* MEMORY MANAGEMENT */
 void	free_paths(char **paths);
 /* UTIL FUNCTIONS */
@@ -56,5 +59,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *prefix, char const *suffix);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_calloc(size_t elements, size_t bytes);
+void	ft_bzero(void *s, size_t n);
 
 #endif
