8c9862d445234195c7b80050683ec37a6d22fd52
Simao <bonvalots@gmail.com>
2023-05-31 11:09:25 +0100

    Main entry

--- main.c
+++ main.c
@@ -5,47 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/27 16:18:53 by simao             #+#    #+#             */
-/*   Updated: 2023/05/28 22:37:13 by simao            ###   ########.fr       */
+/*   Created: 2023/05/29 19:04:07 by simao             #+#    #+#             */
+/*   Updated: 2023/05/31 11:07:06 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
+#include "pipex.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, const char **argv, char **env)
 {
-	int	fd[2];
-	int	pid1;
-	int	pid2;
+	int		i;
+	int		j;
+	char	**paths;
+	char *args[] = {"ls", "-l", NULL};
+	char *envs[] = {NULL};
 
-	if (pipe(fd) == -1)
-		printf("Error on pipe()");
-	pid1 = fork();
-	if (pid1 == 0)
-	{
-		//Child process 1 (ping)
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		execlp("ping", "ping", "-c", "5", "google.com", NULL);
-	}
-	pid2 = fork();
-	if (pid2 == 0)
-	{
-		//child process 2 (grep)
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		execlp("grep", "grep", "rtt", NULL);
-	}
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+	i = 0;
+	j = 0;
+	paths = get_path_list(env);
+	printf("Valor de acesso: %d\n", access("/usr/bin/pwdd", X_OK));
+	//execve("/usr/bin/ls", args, envs);
+	free_paths(paths);
 	return (0);
 }
