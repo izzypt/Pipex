9222f1e8f9b100e2160b3cfc20424f0f948fc633
Simao <bonvalots@gmail.com>
2023-06-12 18:03:44 +0100

    update

--- pipes.c
+++ pipes.c
@@ -6,7 +6,7 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 13:38:10 by simao             #+#    #+#             */
-/*   Updated: 2023/06/12 11:55:53 by simao            ###   ########.fr       */
+/*   Updated: 2023/06/12 18:02:29 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	cmd_pipe(t_list *cmd1, t_list *cmd2)
 	int	pid1;
 	int	pid2;
 
+	pipe(pipe_fd);
 	pid1 = fork();
 	if (pid1 == 0)
 	{
@@ -58,12 +59,8 @@ void	cmd_output_to_file(t_list *cmd, char *file)
 	if (pid1 == 0)
 	{
 		dup2(outfile, STDOUT_FILENO);
-		close(pipe_fd[0]);
-		close(pipe_fd[1]);
 		execve(cmd->path, cmd->full_cmd, NULL);
 	}
-	close(pipe_fd[0]);
-	close(pipe_fd[1]);
 	close(outfile);
 	waitpid(pid1, NULL, 0);
 }
@@ -77,6 +74,7 @@ void	cmd_input_from_file(t_list *cmd, char *file)
 	int	pid1;
 	int	infile;
 
+	pipe(pipe_fd);
 	infile = open(file, O_RDONLY, 0644);
 	pid1 = fork();
 	if (pid1 == 0)
